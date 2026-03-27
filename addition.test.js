1172481076c7bc56266f7bb3b0aa1685d542773d
Ria Bhullar <riabhullar44@gmail.com>
2026-03-27 15:23:30 -0700

    initial commit

---
+++ addition.test.js
@@ -0,0 +1,23 @@
+// The operation should return the sum of 2 numbers
+
+const { addition } = require('./addition')
+
+test('2 + 4 = 6', () => {
+    expect(addition(2,4)).toBe(6)
+})
+
+test('3 + 3 = 6', () => {
+    expect(addition(2,4)).toBe(6)
+})
+
+test.skip('23 + 60 = 83', () => {
+    expect(addition(23,60)).toBe(83)
+})
+
+test.each([[3, 4, 7], [5, -10, -5], [0, 2, 2]])(
+    'a + b = c', (a, b, expected) => {
+        expect(addition(a,b)).toBe(expected)
+    }
+)
+
+// You can do test.skip to have a test avoided in a pull request/test
